33e95b64425d8c2d5c835ec8c2a7fb4d91de17ef
Thomas Toye <toye.thomas@gmail.com>
2014-06-21 20:45:53 +0200

    Remove example test

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -31,12 +31,6 @@ import static org.fest.assertions.Assertions.*;
 public class ApplicationTest {
 
     @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
-    }
-
-    @Test
     public void renderTemplate() {
         Content html = views.html.index.render("Your new application is ready.");
         assertThat(contentType(html)).isEqualTo("text/html");
