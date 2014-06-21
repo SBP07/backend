e58d00fecbdc162f274c1e1fee26fcbd3d28493a
Thomas Toye <toye.thomas@gmail.com>
2014-06-21 19:16:43 +0200

    Add Kind controller
    This controller will handle all logic for Kind'eren
    Currently contains only stub methods, they will be implemented later

---
+++ app/controllers/Kinderen.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import models.*;
+import play.*;
+import play.data.Form;
+import play.mvc.*;
+import views.html.*;
+
+public class Kinderen extends Controller {
+	
+	/**
+	 * Bind form from request and add the new Kind to the database
+	 */
+	public static Result save() {
+		return TODO;
+	}
+	
+	/**
+	 * Show form to create a new Kind
+	 */
+	public static Result nieuw() {
+		return TODO;
+	}
+	
+	/**
+	 * Show (filled) form to edit an existing Kind
+	 * @param	id	The id of the Kind
+	 */
+	public static Result edit(Long id) {
+		return TODO;
+	}
+	
+	/**
+	 * Shows the details of a Kind in a nice table
+	 * @param	id	The id of the Kind
+	 */
+	public static Result details(Long id) {
+		return TODO;
+	}
+	
+	/**
+	 * Show a tabel with all the Kind'eren
+	 */
+	public static Result table() {
+		return TODO;
+	}
+	
+	
+}
