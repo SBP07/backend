55562155f6aaec494587d87cbf86baa732972b25
Thomas Toye <toye.thomas@gmail.com>
2014-06-22 23:46:36 +0200

    Update evolution

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,11 @@
 
 # --- !Ups
 
+create table aanwezigheden (
+  id                        bigint not null,
+  constraint pk_aanwezigheden primary key (id))
+;
+
 create table kind (
   id                        bigint not null,
   voornaam                  varchar(255),
@@ -14,11 +19,16 @@ create table kind (
   geboortedatum             timestamp,
   medische_fiche_in_orde    boolean,
   medische_fiche_gecontroleerd timestamp,
+  aanwezigheden_id          bigint,
   constraint pk_kind primary key (id))
 ;
 
+create sequence aanwezigheden_seq;
+
 create sequence kind_seq;
 
+alter table kind add constraint fk_kind_aanwezigheden_1 foreign key (aanwezigheden_id) references aanwezigheden (id) on delete restrict on update restrict;
+create index ix_kind_aanwezigheden_1 on kind (aanwezigheden_id);
 
 
 
@@ -26,9 +36,13 @@ create sequence kind_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
+drop table if exists aanwezigheden;
+
 drop table if exists kind;
 
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists aanwezigheden_seq;
 
 drop sequence if exists kind_seq;
 
