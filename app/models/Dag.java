d60134164488845622e661969d0b45d27d008229
Thomas Toye <toye.thomas@gmail.com>
2014-06-23 19:23:55 +0200

    Create Dag model

---
+++ app/models/Dag.java
@@ -0,0 +1,20 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.persistence.*;
+
+import play.db.ebean.Model;
+import models.*;
+
+@Entity
+public class Dag extends Model {
+	@Id
+	public Long id;
+	
+	public Date dag;
+	
+	@ManyToMany(mappedBy="voormiddagen")
+	public ArrayList<Kind> voormiddagAanwezigheden = new ArrayList<Kind>();
+}
