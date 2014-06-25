7fa95f3e8c95a22b2f092be20ed79eca630fce05
Thomas Toye <toye.thomas@gmail.com>
2014-06-25 18:57:51 +0200

    Remove unnecessary save()s

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -162,12 +162,8 @@ public class ApplicationTest {
 		    	assertThat(kind.voormiddagen).isNotNull();
 		    	assertThat(kind.voormiddagen.get(0)).isNotNull();
 
-		    	/*dag1.save();
-		    	dag2.save();
-		    	dag3.save();*/
 		    	kind.save();
-		    	//kind.saveManyToManyAssociations("voormiddagen");
-		    	
+		    			    	
 		    	Long id = kind.id;
 		    	
 		    	Kind found = Kind.findById(id);
