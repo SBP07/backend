e25ad51257f1f31faa8262a93cfe72805e8e4798
Thomas Toye <toye.thomas@gmail.com>
2014-06-25 15:02:32 +0200

    Add test for cascading Aanwezigheden

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -125,6 +125,66 @@ public class ApplicationTest {
     	});
     }
     
+    @Test
+    public void aanwezighedenKindCascade() {
+    	running(fakeApplication(), new Runnable() {
+			public void run() {
+		    	Kind kind = new Kind();
+		    	kind.voornaam = "Milan";
+		    	kind.achternaam = "Balcaen";
+		    	
+		    	Date date1 = new Date();
+		    	Date date2 = new Date();
+		    	Date date3 = new Date();
+		    	
+		    	date1.setTime(941500800L);
+		    	date2.setTime(1067731200L);
+		    	date3.setTime(1069286400L);
 
+		    	Dag dag1 = new Dag();
+		    	Dag dag2 = new Dag();
+		    	Dag dag3 = new Dag();
+
+		    	dag1.dag = date1;
+		    	dag2.dag = date1;
+		    	dag3.dag = date1;
+
+		    	kind.voormiddagen.add(dag1);
+		    	dag1.voormiddagAanwezigheden.add(kind);
+		    
+		    	kind.voormiddagen.add(dag2);
+		    	dag2.voormiddagAanwezigheden.add(kind);
+
+		    	kind.voormiddagen.add(dag3);
+		    	dag3.voormiddagAanwezigheden.add(kind);
+		    	
+		    	assertThat(kind).isNotNull();
+		    	assertThat(kind.voormiddagen).isNotNull();
+		    	assertThat(kind.voormiddagen.get(0)).isNotNull();
+
+		    	dag1.save();
+		    	dag2.save();
+		    	dag3.save();
+		    	kind.save();
+		    	
+		    	Long id = kind.id;
+		    	
+		    	Kind found = Kind.findById(id);
+		    	assertThat(found).isNotNull();
+		    	assertThat(found).isNotNull();
+		    	assertThat(found.voormiddagen).isNotNull();
+		    	assertThat(kind.voormiddagen.get(0)).isNotNull();
+		    	assertThat(found.voormiddagen).contains(kind.voormiddagen.get(0));
+		    	assertThat(kind.achternaam).isEqualTo(found.achternaam);
+
+		    	assertThat(kind.voormiddagen.get(0)).isEqualTo(dag1);
+		    	assertThat(kind.voormiddagen.get(1)).isEqualTo(dag2);
+		    	assertThat(kind.voormiddagen.get(2)).isEqualTo(dag3);
+
+		    	assertThat(kind.voormiddagen.size()).isEqualTo(3);
+			}
+    		
+    	});
+    }
 
 }
