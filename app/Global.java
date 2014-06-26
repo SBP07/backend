b09499e8164d8df5c187ec13952068d94ce2eaff
Thomas Toye <toye.thomas@gmail.com>
2014-06-26 14:32:08 +0200

    Add custom StringParser that capitalizes first letter of a word

--- app/Global.java
+++ app/Global.java
@@ -8,6 +8,7 @@ import java.util.*;
 import models.*;
 
 import com.avaje.ebean.*;
+import com.avaje.ebean.text.StringParser;
 import com.avaje.ebean.text.csv.CsvReader;
 
 public class Global extends GlobalSettings {
@@ -26,37 +27,61 @@ public class Global extends GlobalSettings {
 		}
 
 		private static void insertKinderen() {
-			 try {
-				// Volgnummer van het attest;;Naam;Voornaam;Straat en huisnummer;;Postcode en gemeente;Geboortedatum MDY;Monday, April 07, 2014;Tuesday, April 08, 2014;Wednesday, April 09, 2014;Thursday, April 10, 2014;Friday, April 11, 2014
-			     File f = new File("conf/kinderen.csv");
-			 
-			     FileReader reader = new FileReader(f);
-			 
-			     CsvReader<Kind> csvReader = Ebean.createCsvReader(Kind.class);
-			 
-			     csvReader.setPersistBatchSize(20);
-			     
-			     csvReader.addProperty("id");
-			     // ignore the next property
-			     csvReader.addIgnore();
-			     csvReader.addProperty("achternaam");
-			     csvReader.addProperty("voornaam");
-			     csvReader.addProperty("straatEnNummer");
-			     // ignore the next property
-			     csvReader.addIgnore();
-			     csvReader.addProperty("gemeente");
-			     csvReader.addDateTime("geboortedatum", "dd/MM/yyyy");
-			     csvReader.addIgnore();
-			     csvReader.addIgnore();
-			     csvReader.addIgnore();
-			     csvReader.addIgnore();
-			     csvReader.addIgnore();
-			 
-			     csvReader.process(reader);
-			 
-			 } catch (Exception e) {
-			     throw new RuntimeException(e);
-			 }
+			try {
+				// Volgnummer van het attest;;Naam;Voornaam;Straat en
+				// huisnummer;;Postcode en gemeente;Geboortedatum MDY;Monday,
+				// April 07, 2014;Tuesday, April 08, 2014;Wednesday, April 09,
+				// 2014;Thursday, April 10, 2014;Friday, April 11, 2014
+				File f = new File("conf/kinderen.csv");
+
+				FileReader reader = new FileReader(f);
+
+				CsvReader<Kind> csvReader = Ebean.createCsvReader(Kind.class);
+
+				csvReader.setPersistBatchSize(20);
+
+				csvReader.addProperty("id");
+				// ignore the next property
+				csvReader.addIgnore();
+				csvReader.addProperty("achternaam",
+						new CapitalizeStringParser());
+				csvReader.addProperty("voornaam", new CapitalizeStringParser());
+				csvReader.addProperty("straatEnNummer", new CapitalizeStringParser());
+				// ignore the next property
+				csvReader.addIgnore();
+				csvReader.addProperty("gemeente", new CapitalizeStringParser());
+				csvReader.addDateTime("geboortedatum", "dd/MM/yyyy");
+				csvReader.addIgnore();
+				csvReader.addIgnore();
+				csvReader.addIgnore();
+				csvReader.addIgnore();
+				csvReader.addIgnore();
+
+				csvReader.process(reader);
+
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		static class CapitalizeStringParser implements StringParser {
+			/**
+			 * @param value
+			 *            A string
+			 * @return A string that begins with a capital letter
+			 */
+			@Override
+			public Object parse(String value) {
+				return capitalizeFirstLetter(value);
+			}
+
+			public static String capitalizeFirstLetter(String original) {
+				if (original.length() == 0)
+					return original;
+				return original.substring(0, 1).toUpperCase()
+						+ original.substring(1);
+			}
+
 		}
 	}
 }
