e7373fa460ae4a4b89d59e0f62d3b646bbaa4820
Thomas Toye <toye.thomas@gmail.com>
2014-08-02 14:56:29 +0200

    Rewrite hashCode and equals They now both use the id

--- app/models/Kind.java
+++ app/models/Kind.java
@@ -64,22 +64,38 @@ public class Kind extends Model{
 		super.save();
 		super.saveManyToManyAssociations("voormiddagen");
 	}
-	
+
+	/**
+	 * Use id to generate the hashCode
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/**
+	 * Use id to check equality
+	 */
 	@Override
 	public boolean equals(Object obj) {
-		if(obj instanceof Kind) {
-			Kind other = (Kind)obj;
-			if(this.id == null || other.id == null)
-				// even if this.id == other.id == null, the two Kind objects are possibly not equal
-				// we'll let Object.equals(...) handle it, that will return true if they point to the same space in memory
-				return super.equals(other);
-
-			return this.id.equals(other.id);
-		} else {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
 			return false;
-		}
+		if (getClass() != obj.getClass())
+			return false;
+		Kind other = (Kind) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
+
 	/**
 	 * Register an attendance for a Kind in the morning
 	 * @param dag The day to register attendance for
