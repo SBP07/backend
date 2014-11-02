8117a32cbaabb1fc092f4fcc50f54f7e69ee4f7f
Thomas Toye <toye.thomas@gmail.com>
2014-11-02 15:19:53 +0000

    Only add initial data when DB is empty

--- app/Global.scala
+++ app/Global.scala
@@ -11,7 +11,8 @@ object Global extends GlobalSettings {
     import play.api.Play.current
     Logger.info("Application has started")
     play.api.db.slick.DB.withSession{ implicit session =>
-      ChildrenSlick.insert(Child(Some(4), "Thomas", "Toye", Option("0455 55 55 55"), Option("056/55 55 55"), Option("Tomberg 21A"), Option("Beveren-Leie"), Option(new Date()), true, Option(new Date())))
+      if(ChildrenSlick.count == 0)
+        ChildrenSlick.insert(Child(Some(4), "Thomas", "Toye", Option("0455 55 55 55"), Option("056/55 55 55"), Option("Tomberg 21A"), Option("Beveren-Leie"), Option(new Date()), true, Option(new Date())))
     }
     //Child(Some(4), "Thomas", "Toye", Option("0455 55 55 55"), Option("056/55 55 55"), Option("Tomberg 21A"), Option("Beveren-Leie"), Option(new Date()), true, Option(new Date())),
     //Child(None, "Jan", "Doe", Option("+324 55 88 99 22"), Option("554 2245 58"), Option("Straatlaan 55"), Option("Boeregat"), Option(new Date()), false, Option(new Date())
--- app/models/Child.scala
+++ app/models/Child.scala
@@ -50,6 +50,7 @@ object ChildrenSlick {
   def findById(id: Long)(implicit s: Session): Option[Child] = children.filter(_.id === id).firstOption
   def findAll(implicit s: Session) = children.list
   def insert(child: Child)(implicit s: Session) = children.insert(child)
+  def count(implicit s: Session) = children.length.run
   def update(child: Child)(implicit s: Session) = {
     child.id match {
       case Some(id) => children.filter(_.id === id).update(child)
