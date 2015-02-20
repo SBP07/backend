65afcdf33e3039869d4bc3fc35193e9158a26f05
Thomas Toye <toye.thomas@gmail.com>
2015-02-20 15:33:59 +0100

    Split update and registering presences

--- app/controllers/Presences.scala
+++ app/controllers/Presences.scala
@@ -30,8 +30,53 @@ object Presences extends Controller {
     }
   )
 
+  def updateWithId(childId: Long) = DBAction { implicit s =>
+    val child = ChildrenModel.findById(childId)
+    val presences = child.flatMap(_.id).map(ChildPresences.findAllForChild(_)).map(_.map(_._1)).map(_.toList).getOrElse(Nil)
+    child match {
+      case Some(c) => Ok(
+        views.html.presences.updatePresencesForm.render(registerForm.fill(PresencesPost(child, presences, ActivitiesModel.findAllWithType.toList.map(_._2))),
+          c, LocalDate.now, ActivitiesModel.findAllWithType.toList, s.flash)
+      )
+      case _ => BadRequest("Kind niet gevonden")
+    }
+  }
+
+  def saveUpdate = DBAction { implicit rs =>
+    registerForm.bindFromRequest.fold(
+      formWithErrors => {
+        formWithErrors.value.flatMap(_.child).flatMap(_.id.flatMap(ChildrenModel.findById(_))) match {
+          case Some(child) => BadRequest (views.html.presences.updatePresencesForm.render(formWithErrors, child, LocalDate.now, ActivitiesModel.findAllWithType.toList, rs.flash) )
+          case _ => BadRequest("Kind niet gevonden")
+        }
+      },
+      _ match {
+        case PresencesPost(None, _, _) => BadRequest("Ongeldig kind");
+        case PresencesPost(Some(child), selectedActivities, possibleActivities) => {
+          child.id match {
+            case Some(id) => rs.dbSession.withTransaction {
+              val alreadyPersisted = ChildPresences.findAllForChild(id).map(_._1).toList
+
+              val toPersist = PresencesPost.presencesToInsert(selectedActivities, possibleActivities, alreadyPersisted)
+              val toDelete = PresencesPost.presencesToDelete(selectedActivities, possibleActivities, alreadyPersisted)
+
+              ChildPresences.register(toPersist.map(_.id).flatten.map(ChildPresence(id, _)))
+              ChildPresences.unregister(toDelete.map(_.id).flatten.map(ChildPresence(id, _)))
+
+              Redirect(routes.Children.details(id)).flashing("success" -> "Aanwezigheden upgedated")
+            }
+
+            case _ => BadRequest("Ongeldig kind")
+          }
+
+        }
+      }
+
+    )
+  }
+
   def register = DBAction { implicit s =>
-    Ok(views.html.presences.register.render(registerForm, ChildrenModel.findAll, LocalDate.now, ActivitiesModel.findAllWithType.toList, s.flash))
+    Ok(views.html.presences.register.render(registerForm.fill(PresencesPost(None, Nil, ActivitiesModel.findAllWithType.toList.map(_._2))), ChildrenModel.findAll, LocalDate.now, ActivitiesModel.findAllWithType.toList, s.flash))
   }
 
   def registerWithId(childId: Long) = DBAction { implicit s =>
@@ -54,12 +99,10 @@ object Presences extends Controller {
               val alreadyPersisted = ChildPresences.findAllForChild(id).map(_._1).toList
 
               val toPersist = PresencesPost.presencesToInsert(selectedActivities, possibleActivities, alreadyPersisted)
-              val toDelete = PresencesPost.presencesToDelete(selectedActivities, possibleActivities, alreadyPersisted)
 
               ChildPresences.register(toPersist.map(_.id).flatten.map(ChildPresence(id, _)))
-              ChildPresences.unregister(toDelete.map(_.id).flatten.map(ChildPresence(id, _)))
 
-              Redirect(routes.Children.details(id)).flashing("success" -> "Aanwezigheden toegevoegd")
+              Redirect(routes.Children.details(id)).flashing("success" -> s"${toPersist.size} aanwezigheden toegevoegd")
             }
 
             case _ => BadRequest("Ongeldig kind")
