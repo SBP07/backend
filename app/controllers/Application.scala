9b22e65b356609d350700643cb81642d4d31ed85
Thomas Toye <toye.thomas@gmail.com>
2015-02-25 12:50:33 +0100

    Add simple home page

---
+++ app/controllers/Application.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import java.util.Date
+
+import org.joda.time.{DateTimeZone, LocalDate}
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.format.Formats._
+import play.api.db.slick._
+
+import views._
+import models._
+import models.{Children => ChildrenModel}
+
+object Application extends Controller {
+  def home = Action { implicit request =>
+    Ok(views.html.application.home.render(request.flash))
+  }
+}
