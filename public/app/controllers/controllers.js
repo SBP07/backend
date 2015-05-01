3064842428a1dcbd97674e0c52f34fb803728023
Thomas Toye <toye.thomas@gmail.com>
2015-05-01 22:56:13 +0200

    Sow details for children and animators
    Also use standard controllers naming (first letter capitalised)

--- public/app/controllers/controllers.js
+++ public/app/controllers/controllers.js
@@ -1,24 +1,119 @@
 var speelsysteemControllers = angular.module('speelsysteemControllers', ['ui.bootstrap']);
 
-speelsysteemControllers.controller('animatorsController', function($scope, $http) {
+speelsysteemControllers.controller('AnimatorsController', function($scope, $http, $modal) {
     $http.get('/api/animator/all')
-        .success(function (response) {$scope.animators = response;});
+        .success(function (response) { $scope.animators = response; });
+
+    $scope.animatorDetails = function (animator) {
+        var modalInstance = $modal.open({
+            templateUrl: '/assets/app/templates/animator/details.html',
+            controller: 'AnimatorDetailsController',
+            size: "lg",
+            scope: $scope,
+            resolve: {
+                animator: function() { return animator; }
+            }
+        });
+    };
 });
 
-speelsysteemControllers.controller('shiftsController', function($scope, $http) {
+speelsysteemControllers.controller('AnimatorDetailsController', function ($scope, $modalInstance, $modal, animator) {
+    $scope.animator = animator;
+
+    $scope.modalWrapper = {
+        include: '/assets/app/templates/animator/form.html',
+        title: 'Animator bewerken'
+    };
+
+    $scope.edit = function (animator) {
+        var modalInstance = $modal.open({
+            templateUrl: '/assets/app/templates/misc/modalWrapper.html',
+            controller: 'AnimatorFormController',
+            size: "lg",
+            scope: $scope,
+            resolve: {
+                animator: function () {
+                    return animator;
+                }
+            }
+        });
+    };
+
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
+});
+
+speelsysteemControllers.controller('ShiftsController', function($scope, $http) {
     $http.get('/api/shift/all')
-        .success(function (response) {$scope.shifts = response;});
+        .success(function (response) { $scope.shifts = response; });
 });
 
-speelsysteemControllers.controller('childrenController', function($scope, $http) {
+speelsysteemControllers.controller('ChildrenController', function($scope, $http, $modal) {
     $http.get('/api/child/all')
-        .success(function (response) {$scope.children = response;});
+        .success(function (response) { $scope.children = response; });
+
+    $scope.details = function (child) {
+        var modalInstance = $modal.open({
+            templateUrl: '/assets/app/templates/child/details.html',
+            controller: 'ChildDetailsController',
+            size: "lg",
+            scope: $scope,
+            resolve: {
+                child: function () { return child; }
+            }
+        });
+    };
 });
 
-speelsysteemControllers.controller('homeController', function() {
+speelsysteemControllers.controller('ChildDetailsController', function ($scope, $modal, child) {
+    $scope.child = child;
+
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
+
+    $scope.modalWrapper = {
+        include: '/assets/app/templates/child/form.html',
+        title: 'Kind bewerken'
+    };
+
+    $scope.edit = function (child) {
+        var modalInstance = $modal.open({
+            templateUrl: '/assets/app/templates/misc/modalWrapper.html',
+            controller: 'ChildFormController',
+            size: "lg",
+            scope: $scope,
+            resolve: {
+                child: function () {
+                    return angular.copy(child);
+                }
+            }
+        });
+    };
+});
+
+speelsysteemControllers.controller('HomeController', function() {
 
 });
 
-speelsysteemControllers.controller('navbarController', function() {
+speelsysteemControllers.controller('NavbarController', function() {
 
+});
+
+speelsysteemControllers.controller('ChildFormController', function($scope, child) {
+    $scope.child = child || {};
+
+    $scope.format = "dd-MM-yyyy";
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+    $scope.saveChild = function() {
+        console.log($scope.child);
+    };
 });
--- public/app/main.js
+++ public/app/main.js
@@ -3,16 +3,19 @@ var app = angular.module('speelsysteemApp', ['ngRoute', 'speelsysteemControllers
 app.config(function($routeProvider){
     $routeProvider.when('/', {
         templateUrl: 'assets/app/templates/home.html',
-        controller: 'homeController'
+        controller: 'HomeController'
     }).when('/animatoren', {
         templateUrl: 'assets/app/templates/animators.html',
-        controller: 'animatorsController'
+        controller: 'AnimatorsController'
     }).when('/kinderen', {
         templateUrl: 'assets/app/templates/children.html',
-        controller: 'childrenController'
+        controller: 'ChildrenController'
     }).when('/dagdelen', {
         templateUrl: 'assets/app/templates/shifts.html',
-        controller: 'shiftsController'
+        controller: 'ShiftsController'
+    }).when('/kinderen/nieuw', {
+        templateUrl: 'assets/app/templates/child/form.html',
+        controller: 'ChildFormController'
     }).otherwise({
         redirectTo: '/'
     });
