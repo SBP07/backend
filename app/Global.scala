f719bb3034b4c514f21865fec848564b587cf449
Thomas Toye <toye.thomas@gmail.com>
2015-05-25 11:14:02 +0100

    Remove csv importers

--- app/Global.scala
+++ app/Global.scala
@@ -1,9 +1,6 @@
-import helpers.CsvImporters
-import models._
+import com.softwaremill.macwire.Macwire
 import models.repository._
 import play.api._
-import play.api.db.slick.Config.driver.simple.Session
-import com.softwaremill.macwire.Macwire
 
 
 object Global extends GlobalSettings with Macwire {
@@ -15,54 +12,4 @@ object Global extends GlobalSettings with Macwire {
   val animatorRepository = wire[SlickAnimatorRepository]
   val shiftRepository = wire[ShiftRepository]
 
-  // scalastyle:off
-  override def onStart(app: Application) {
-    import play.api.Play.current
-    play.api.db.slick.DB.withSession { implicit session =>
-      insertChildren
-      insertShiftTypes
-      insertShifts
-      insertAnimators
-      insertChildPresences
-    }
-
-  }
-
-  private def insertChildPresences(implicit s: Session) {
-    if (ChildPresenceRepository.count == 0) {
-      CsvImporters.childPresences("conf/initial_data/child_presences.csv", childRepository, shiftRepository)
-        .foreach(ChildPresenceRepository.register)
-    }
-  }
-
-  private def insertAnimators(implicit s: Session) {
-    if (animatorRepository.count == 0) {
-      helpers.CsvImporters
-        .animators("conf/initial_data/animators.csv")
-        .foreach(animatorRepository.insert)
-    }
-  }
-
-  def insertShifts(implicit s: Session) {
-    if (shiftRepository.count == 0) {
-      CsvImporters.shifts("conf/initial_data/shifts.csv").foreach(shiftRepository.insert)
-    }
-  }
-
-  private def insertShiftTypes(implicit s: Session) {
-    if (ShiftTypeRepository.count == 0) {
-      ShiftTypeRepository.insert(ShiftType(None, "VM", "Voormiddag"))
-      ShiftTypeRepository.insert(ShiftType(None, "NM", "Namiddag"))
-      ShiftTypeRepository.insert(ShiftType(None, "MID", "Middag"))
-      ShiftTypeRepository.insert(ShiftType(None, "EXT", "Externe activitieit (niet op het speelplein)"))
-    }
-  }
-
-  private def insertChildren(implicit s: Session) {
-    if (childRepository.count == 0) {
-      CsvImporters.children("conf/initial_data/children.csv").foreach(childRepository.insert)
-    }
-  }
-
-  // scalastyle:on
 }
