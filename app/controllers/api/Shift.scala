29b339493ffc771a7f5ca55bab623a0b1bae5d25
Thomas Toye <toye.thomas@gmail.com>
2015-11-10 12:21:06 +0100

    Add stub controllers for API

---
+++ app/controllers/api/Shift.scala
@@ -0,0 +1,12 @@
+package controllers.api
+
+import play.api._
+import play.api.mvc._
+
+class Shift extends Controller {
+  def update = TODO
+  def delete(id: Long): Action[AnyContent] = TODO
+  def getById(id: Long): Action[AnyContent] = TODO
+  def getAll: Action[AnyContent] = TODO
+  def create = TODO
+}
