8a6fb2607f6581a7cdec1d7a20fb1414768b9589
Thomas Toye <toye.thomas@gmail.com>
2015-11-17 22:26:28 +0100

    Small clean-up

--- app/dao/RepoFor.scala
+++ app/dao/RepoFor.scala
@@ -2,7 +2,7 @@ package dao
 
 import io.strongtyped.active.slick._
 
-trait RepoFor[M, I] extends EntityActions with PostgresProfileProvider { // TODO break this out in seperate file
+trait RepoFor[M, I] extends EntityActions with PostgresProfileProvider {
   type Entity = M
-  type Id = I // TODO Make this UUID
+  type Id = I
 }
