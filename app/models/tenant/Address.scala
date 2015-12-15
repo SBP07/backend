9d28b7307dbcfba221b1c6346083c1c29852be98
Thomas Toye <toye.thomas@gmail.com>
2015-12-15 18:20:53 +0100

    Formatting

--- app/models/tenant/Address.scala
+++ app/models/tenant/Address.scala
@@ -1,12 +1,8 @@
 package models.tenant
 
-import java.util.UUID
-
-import models.helpers.BelongsToTenant
-
 case class Address(
-                    street: String,
-                    zipCode: Int,
-                    city: String,
-                    country: String
-                  )
+  street: String,
+  zipCode: Int,
+  city: String,
+  country: String
+)
