f99d1189295affe1e2f26d36297d0a315eebbc80
Thomas Toye <toye.thomas@gmail.com>
2016-08-09 18:43:39 +0200

    Implement Ordered[DayDate] trait

--- models/src/main/scala/be/thomastoye/speelsysteem/models/DayDate.scala
+++ models/src/main/scala/be/thomastoye/speelsysteem/models/DayDate.scala
@@ -3,7 +3,10 @@ package be.thomastoye.speelsysteem.models
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-case class DayDate(day: Int, month: Int, year: Int) {
+case class DayDate(day: Int, month: Int, year: Int) extends Ordered[DayDate] {
   def toLocalDate: LocalDate = LocalDate.of(year, month, day)
+
   override def toString = toLocalDate.format(DateTimeFormatter.ISO_LOCAL_DATE)
+
+  override def compare(that: DayDate): Int = this.toLocalDate.compareTo(that.toLocalDate)
 }
